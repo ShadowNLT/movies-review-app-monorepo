02fd6b2a2174f9dc6b5f2617ce99383935219759
NLT <Neila.Lekane-Tamba@adesso.de>
2025-05-20 20:47:24 +0200

    feat: return JSON Response from healthcheck endpoint

--- backend/cmd/api/healthcheck.go
+++ backend/cmd/api/healthcheck.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Status: available")
-	fmt.Fprintf(w, "Environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "Version: %s\n", appVersion)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     appVersion,
+	}
+	jsonValue, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	// Add a new line at the end to make it easier to view in terminal applications
+	jsonValue = append(jsonValue, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(jsonValue)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 }
