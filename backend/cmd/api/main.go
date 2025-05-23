c970fc00903fffb375d6818d30c75c98e5638025
NLT <Neila.Lekane-Tamba@adesso.de>
2025-05-23 22:44:33 +0200

    feat: Add and Configure Database Connection

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log/slog"
 	"net/http"
 	"os"
@@ -14,6 +17,12 @@ const appVersion = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
@@ -27,10 +36,32 @@ func main() {
 	// Read the command-line flags values into the config struct
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("CINEPULSE_DB_DSN"), "PostgreSQL DSN")
+
+	// Database Connection Pool settings
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max idle connections")
 	flag.Parse()
 
 	// Initialize a new structured logger which writes log entries to the standard out stream
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// Make sure the connection is closed before the main() function exists
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			logger.Error(err.Error())
+		}
+	}(db)
+
+	logger.Info("Database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -48,9 +79,34 @@ func main() {
 
 	logger.Info("Starting server", "addr", srv.Addr, "env", cfg.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		dbCloseErr := db.Close()
+		if dbCloseErr != nil {
+			return nil, dbCloseErr
+		}
+		return nil, err
+	}
+
+	return db, nil
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module cinepulse.nlt.net
 
 go 1.24.0
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
