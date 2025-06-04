bf7d5ac830709a6e407c07e4bf1b059445ca4e18
NLT <Neila.Lekane-Tamba@adesso.de>
2025-06-05 00:12:41 +0200

    feat: catching SIGINT and SIGTERM signals

--- backend/cmd/api/server.go
+++ backend/cmd/api/server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -17,7 +22,39 @@ func (app *application) serve() error {
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+		app.logger.Info("caught signal", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		// Gracefully shutdown the server
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
 
-	return srv.ListenAndServe()
+	// Calling Shutdown() on our server will cause ListenAndServe to immediately return a
+	// http.ErrServerClosed error. So if we see this error, it is actually a good thing and an
+	// indication that the graceful shutdown has started.
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("server stopped", "addr", srv.Addr)
+
+	return nil
 }
